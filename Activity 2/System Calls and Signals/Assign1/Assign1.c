1e095369c617782f001212a2cf0e4a18b8646505
99003553 <78540508+99003553@users.noreply.github.com>
2021-03-04 23:17:17 +0530

    Create Assign1.c

---
+++ Activity 2/System Calls and Signals/Assign1/Assign1.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+int main() 
+{ 
+    FILE *fptr, *fptr1; 
+    char filenme[100], d; 
+  
+    printf("Enter the filename for reading \n"); 
+    scanf("%s", filenme); 
+    // Open one file for reading 
+    fptr = fopen(filenme, "r"); 
+    if (fptr == NULL) 
+    { 
+        printf("Cannot open file %s \n", filenme); 
+        exit(0); 
+    } 
+  
+    printf("Enter the filename to open for writing \n"); 
+    scanf("%s", filenme); 
+  
+    // Open another file for writing 
+    fptr1 = fopen(filenme, "w"); 
+    if (fptr1 == NULL) 
+    { 
+        printf("Cannot open file %s \n", filenme); 
+        exit(0); 
+    } 
+  
+    // Read contents from file 
+    d = fgetc(fptr); 
+    while (c != EOF) 
+    { 
+        fputc(d, fptr1); 
+        d = fgetc(fptr); 
+    } 
+  
+    printf("\nContents copied to %s", filenme); 
+  
+    fclose(fptr); 
+    fclose(fptr1); 
+    return 0; 
+}
